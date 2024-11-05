54d30b9cd968cdcd8564aa7f8a6bc998e94e02d1
Justin Gentry <justingentry@outlook.com>
2024-11-05 12:37:34 -0600

    final push

--- vault.js
+++ vault.js
@@ -1,1 +1,21 @@
-//Pseudocode: I will make three varables to make the calculations necessary to provide the lock combination
+//Pseudocode: 1. I will make a repository for this assignment in Github. 2. Clone the resporitory to my block07 folder in VS Code. 3. Make a js file and html file within my git clone. 4. Add, commit and inital push of js file to Github repository. 4. Make and declare three varables to make the calculations necessary to provide the lock combination. 5. Have comments throughtout explaining my steps. 6. Create dialog box to display the valut code. 7. Add js file to html file. 8. Final push to repository. 9.Submit Github repo link
+
+const greeting =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+console.log(greeting);
+//into message
+
+var num1 = 5 + 5;
+console.log(num1);
+//first combination number: 10
+
+var num2 = 4 * 10;
+console.log(num2);
+//second combination number: 40
+
+var num3 = 40 - 1;
+console.log(num3);
+//third combination number: 39
+
+alert(num1, num2, num3);
+//combined combination numbers
